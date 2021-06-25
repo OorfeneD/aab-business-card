3565180cdf5e669c6ab6eda06cb9e9a461774f1e
oorfened <oorfened@gmail.com>
2021-06-26 00:45:56 +0300

    fixes: link in footer and contacts for footer images added <a> with correct href for contact position added Yandex.Karty url instead of Google Maps

--- components/ContactList.vue
+++ components/ContactList.vue
@@ -16,7 +16,7 @@
         dr.amina.ask@gmail.com
       </p>
     </a>
-    <a href="http://www.google.com/maps/place/Волгоградский+пр-т.,+168,+Москва,+Россия,+109472/" target="_blank" class="w-full h-12 my-1 mb-5 px-5 flex items-stretch justify-center">
+    <a href="https://yandex.ru/maps/213/moscow/search/ГБУЗ%20«Госпиталь%20Ветеранов%20Войн%202»%20ДЗМ%20Волгоградский%20проспект%2C%20168%2C%20строение%201%2C%20Москва/" target="_blank" class="w-full h-12 my-1 mb-5 px-5 flex items-stretch justify-center">
       <img src="~assets/icons/icon-map.svg" alt="" class="inline-block rounded-xl h-12 w-12">
       <p class="inline-flex text-green-800 font-bold bg-green-100 w-full items-center px-1 text-xs">
         Москва, Волгоградский проспект, дом 168, строение 1
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="flex flex-col bg-bottom w-full mt-10 items-center pb-1">
     <div class="flex justify-between w-64 items-center">
-      <img src="~assets/icons/icon-mobile-sm.svg" alt="" class="w-12 h-12 rounded-xl bg-green-900 sq-shadow">
-      <img src="~assets/icons/icon-email-sm.svg" alt="" class="w-12 h-12 rounded-xl bg-green-900 sq-shadow">
-      <img src="~assets/icons/icon-inst.svg" alt="" class="w-12 h-12 rounded-xl bg-green-900 sq-shadow">
-      <img src="~assets/icons/icon-facebook.svg" alt="" class="w-12 h-12 rounded-xl bg-green-900 sq-shadow">
+      <a href="tel:+7 (926) 216-05-58">
+        <img src="~assets/icons/icon-mobile-sm.svg" alt="" class="w-12 h-12 rounded-xl bg-green-900 sq-shadow">
+      </a>
+      <a href="mailto:dr.amina.ask@gmail.com">
+        <img src="~assets/icons/icon-email-sm.svg" alt="" class="w-12 h-12 rounded-xl bg-green-900 sq-shadow">
+      </a>
+      <a href="http://instagram.com/dr_amina_askerova/" target="_blank" rel="noopener noreferrer">
+        <img src="~assets/icons/icon-inst.svg" alt="" class="w-12 h-12 rounded-xl bg-green-900 sq-shadow">
+      </a>
+      <a href="http://www.facebook.com/amina.askerova.7" target="_blank" rel="noopener noreferrer">
+        <img src="~assets/icons/icon-facebook.svg" alt="" class="w-12 h-12 rounded-xl bg-green-900 sq-shadow">
+      </a>
     </div>
     <div class="text-xs text-gray-600 mt-3">
       (c) OorfeneD and Detra
