fcca1744af9ec44187fc3fe6cd66a3a4ae540dc6
oorfened <oorfened@gmail.com>
2021-06-26 16:07:50 +0300

    fix notification visibility

--- components/NotificationsProvider.vue
+++ components/NotificationsProvider.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute top-0 left-0 w-screen h-0 pointer-events-none">
+  <div class="absolute top-0 left-0 w-screen h-screen pointer-events-none">
     <notification v-for="notification, index in notifications" :key="index">
       {{ notification.text }}
     </notification>
