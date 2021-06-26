88cdea6dec537a5fbf898183bcfe57ae8b1b90ad
oorfened <oorfened@gmail.com>
2021-06-26 14:17:15 +0300

    added LogoName component replaced text in header also some responsibility improvements in header

--- components/Header.vue
+++ components/Header.vue
@@ -1,44 +1,23 @@
 <template>
-  <div class="flex justify-center items-center flex-col w-full pt-5 mb-5">
-    <div class="w-40 h-40 rounded-full border-8 border-white shadow-lg overflow-hidden">
+  <div class="flex justify-center items-center flex-col w-full md:w-auto pt-5 mb-5">
+    <div class="w-40 h-40 md:w-72 md:h-72 rounded-full border-8 border-white shadow-lg overflow-hidden md:mb-20 lg:mb-0">
       <img src="~assets/images/logo.jpg" alt="Logo image" class="">
     </div>
-    <p class="font-bold text-xl text-green-700 mt-3 sq-name">
-      Аскерова Амина Джамаловна
-    </p>
-    <p class="">
-      Врач-акушер-гинеколог
-    </p>
+    <logo-name class="md:hidden" />
   </div>
 </template>
 
 <script>
+import LogoName from './LogoName.vue'
 export default {
-
+  components: { LogoName }
 }
 </script>
 
 <style scoped>
-@font-face {
-  font-family: 'Kontora bolder';
-  src: url('~assets/fonts/Kontora Bold.otf');
-}
-@font-face {
-  font-family: 'Kontora';
-  src: url('~assets/fonts/Kontora SemiBold.otf');
-}
-@font-face {
-  font-family: 'OpenSans';
-  src: url('~assets/fonts/OpenSans-Regular.ttf');
-}
+
 img {
   width: 100%;
   height: auto;
-}
-p {
-  font-family: 'OpenSans' sans-serif;
-}
-.sq-name {
-  font-family: 'Kontora bolder';
 }
 </style>
---
+++ components/LogoName.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full flex flex-col justify-center items-center">
+    <p class="font-bold text-xl md:text-2xl text-green-700 mt-3 sq-name md:w-96">
+      Аскерова Амина Джамаловна
+    </p>
+    <p class="md:mb-5 md:text-lg">
+      Врач-акушер-гинеколог
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@font-face {
+  font-family: 'Kontora bolder';
+  src: url('~assets/fonts/Kontora Bold.otf');
+}
+@font-face {
+  font-family: 'Kontora';
+  src: url('~assets/fonts/Kontora SemiBold.otf');
+}
+@font-face {
+  font-family: 'OpenSans';
+  src: url('~assets/fonts/OpenSans-Regular.ttf');
+}
+p {
+  font-family: 'OpenSans' sans-serif;
+}
+.sq-name {
+  font-family: 'Kontora bolder';
+}
+</style>
