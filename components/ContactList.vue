f176d773895943844364d65c2aa00f57a69c2d60
oorfened <oorfened@gmail.com>
2021-06-26 12:11:01 +0300

    added: ContactRow component ContactRow replaces <a> tag with contact info in ContactList

--- components/ContactList.vue
+++ components/ContactList.vue
@@ -1,35 +1,29 @@
 <template>
   <div class="flex flex-col w-72 items-center">
     <Divider />
-    <p class="mt-5">
+    <p class="mt-2 mb-3">
       Контактная информация:
     </p>
-    <a href="tel:+7 (926) 216-05-58" class="w-full h-10 my-1 mt-5 px-5 flex items-stretch justify-center">
-      <img src="~assets/icons/icon-mobile-sm.svg" alt="" class="inline-block rounded-xl h-10 w-10">
-      <p class="inline-flex text-green-800 font-bold bg-green-100 w-full items-center px-3">
-        +7 (926) 216-05-58
-      </p>
-    </a>
-    <a href="mailto:dr.amina.ask@gmail.com" class="w-full h-10 my-1 px-5 flex items-stretch">
-      <img src="~assets/icons/icon-email-sm.svg" alt="" class="inline-block rounded-xl h-10 w-10">
-      <p class="inline-flex text-green-800 font-bold bg-green-100 w-full items-center px-3">
-        dr.amina.ask@gmail.com
-      </p>
-    </a>
-    <a href="https://yandex.ru/maps/213/moscow/search/ГБУЗ%20«Госпиталь%20Ветеранов%20Войн%202»%20ДЗМ%20Волгоградский%20проспект%2C%20168%2C%20строение%201%2C%20Москва/" target="_blank" class="w-full h-12 my-1 mb-5 px-5 flex items-stretch justify-center">
-      <img src="~assets/icons/icon-map.svg" alt="" class="inline-block rounded-xl h-12 w-12">
-      <p class="inline-flex text-green-800 font-bold bg-green-100 w-full items-center px-1 text-xs">
-        Москва, Волгоградский проспект, дом 168, строение 1
-      </p>
-    </a>
+    <contact-row href="tel:+79262160558" w-full :src="require('@/assets/icons/icon-mobile-sm.svg')" text="+7 (926) 216-05-58" />
+    <contact-row href="mailto:dr.amina.ask@gmail.com" w-full :src="require('@/assets/icons/icon-email-sm.svg')" text="dr.amina.ask@gmail.com" />
+    <contact-row
+      href="https://yandex.ru/maps/213/moscow/search/ГБУЗ%20«Госпиталь%20Ветеранов%20Войн%202»%20ДЗМ%20Волгоградский%20проспект%2C%20168%2C%20строение%201%2C%20Москва/"
+      w-full
+      big
+      text-small
+      :src="require('@/assets/icons/icon-map.svg')"
+      text="Москва, Волгоградский проспект, дом 168, строение 1"
+    />
+    <div class="w-full my-1 h-1" />
     <Divider />
   </div>
 </template>
 
 <script>
+import ContactRow from './ContactRow.vue'
 import Divider from './Divider.vue'
 export default {
-  components: { Divider }
+  components: { Divider, ContactRow }
 }
 </script>
 
---
+++ components/ContactRow.vue
@@ -0,0 +1,45 @@
+<template>
+  <a :href="href" :class="`${wFull ? 'w-full' : ''} ${!big? 'h-10' : 'h-12'} my-1 mt-1 mx-3 rounded-xl bg-green-100 flex items-stretch justify-center`">
+    <img :src="src" alt="" :class="`inline-block rounded-xl ${!big? 'h-10 w-10' : 'h-12 w-12'} transform scale-110`">
+    <p :class="`inline-flex text-green-800 font-bold  w-full items-center ${ textSmall ? 'px-1 text-xs' : 'px-3'}`">
+      {{ text }}
+    </p>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    href: {
+      type: String,
+      default: ''
+    },
+    src: {
+      type: String,
+      default: ''
+    },
+    big: {
+      type: Boolean,
+      default: false
+    },
+    wFull: {
+      type: Boolean,
+      default: true
+    },
+    textSmall: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  z-index: 2;
+}
+</style>
