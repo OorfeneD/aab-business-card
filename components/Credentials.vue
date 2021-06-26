78e092ad89792650698adf56179613efaf41ed64
oorfened <oorfened@gmail.com>
2021-06-26 14:18:35 +0300

    added Credentials component

---
+++ components/Credentials.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-xs text-white md:text-gray-600 p-1">
+    (c) OorfeneD and Detra
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
